1908ecd0d6262af5e35a5e2a0e5d3b79bc54e6a5
alan hawkins <hawk.alan@gmail.com>
2014-03-18 18:10:52 +1100

    added division and logarithm

--- arithmatic.hs
+++ arithmatic.hs
@@ -8,11 +8,19 @@ dec = (subtract 1)
 add 0 b = b
 add a b = add (dec a) (inc b)
 
-multiply a 0 = 0
+multiply _ 0 = 0
 multiply a b = add a (multiply a (dec b))
+
+division _ 0 = error "divide by zero error"
+division a 1 = 1
+division a n | a > 0 = 1 + division (a-n) n
+             | otherwise = 0 
 
 power a 0 = 1
 power a b = multiply a (power a (dec b))
 
+logarithm _ 0 = 1 
+logarithm _ 1 = 0 
+logarithm base number = 1 + logarithm 10  (division number base)
 
 liveMain =  print $ power 5 5
