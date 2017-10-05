78997dae3779681b081d4244422c46c190ee7365
MatrixMike <matroxmike@icloud.com>
2017-10-05 20:54:39 +1100

    changed module name to reflect the directory structure

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 {-# OPTIONS_GHC -fwarn-missing-signatures #-}
-import Math.Arithmatic
+-- changed module name to reflect the directory structure
+import Math2.Arithmatic
 -- 05.10.2017 20:40:51
 -- create a small Main to use the module Math.Arithmatic
 
